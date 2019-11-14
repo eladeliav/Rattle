36ebd67014c7245d0ca67eb259e216240b43b247
Elad Eliav <elad02.eliav@gmail.com>
2019-11-15 01:02:35 +0200

    Added file reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,34 @@
 #include "Lexer.hpp"
 #include "Parser.hpp"
 #include "Interpreter.hpp"
+#include <fstream>
 
-int main()
+int main(int argc, char** argv)
 {
+    if(argc == 2)
+    {
+        // got file, try to open
+        std::ifstream ifs(argv[1]);
+        std::string line;
+        while(std::getline(ifs, line))
+        {
+            if(line == "quit()" || line == "exit()")
+                exit(0);
+
+            Lexer lexer(line);
+            Parser parser(lexer);
+            Interpreter interpreter(parser);
+            interpreter.interpret();
+        }
+        exit(0);
+    }
     while(true)
     {
         std::string input;
         std::getline(std::cin, input);
 
         if(input == "quit()" || input == "exit()")
-            break;
+            exit(0);
 
         Lexer lexer(input);
         Parser parser(lexer);
